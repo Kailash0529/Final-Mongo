3e3fdf3c2bfd2520028161f6f3d4efe4b9e3fbcb
kailashjoshi <joshikailash0529@gmail.com>
2024-08-25 10:32:43 +0530

    student degeneration completed

--- src/main/java/semi/minor/pro/entity/Student.java
+++ src/main/java/semi/minor/pro/entity/Student.java
@@ -1,13 +1,14 @@
 package semi.minor.pro.entity;
 
 import lombok.Data;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+//import javax.persistence.Entity;
+
 import java.io.Serializable;
-@Entity
-@Document
+
+@Document(collection = "student")
 @Data
 public class Student implements Serializable {
     @Id
